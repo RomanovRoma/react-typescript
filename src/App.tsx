d75cfd2b6191d6ef9b8b951aa227492ecd084d36
RomanovRoma <romanian.reget@gmail.com>
2021-02-08 20:42:08 +0300

    add typing of class components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// const Title:React.FС<{title: string}> = ({title, children}) => <h2>{title}{children}</h2>
-
-type TitleProps = {
-  title: string,
-  test?: string,
+type CounterState = {
+  count: number
 }
 
-const Title = ({title, test = 'test'}: TitleProps) => <h2>{title}{test}</h2>
+type CounterProps = {
+  title?: string,
+}
 
-const App = () => <Title title="test"/>
+class Counter extends Component<CounterProps, CounterState> {
+
+  constructor(props: CounterProps) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  static defaultProps: CounterProps = {
+    title: "Default counter:"
+  }
+
+  static getDerivedStateFromProps(props: CounterProps, state: CounterState): CounterState | null {
+    return false ? { count: 2 } : null
+  }
+
+  componentDidMount(): void {
+
+  }
+
+  shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState): boolean {
+    return true
+  }
+
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: ++count
+    }))
+
+    // this.state.count = ++count
+  }
+
+  render() {
+    // this.props.title = ''
+
+    return (
+      <div>
+        <h1>{this.props.title}{this.state.count}</h1>
+        <button onClick={this.handleClick}>+1</button>
+      </div>
+    )
+  }
+}
+
+const App = () => <Counter title="Counter:" />
 
 export default App;
