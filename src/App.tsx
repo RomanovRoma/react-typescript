d0d9454c76dfd7430b13f1f2ec2a4cbddfedc7bd
RomanovRoma <romanian.reget@gmail.com>
2021-02-08 14:41:51 +0300

    add typing of functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const Title:React.FС<{title: string}> = ({title, children}) => <h2>{title}{children}</h2>
+
+type TitleProps = {
+  title: string,
+  test?: string,
 }
+
+const Title = ({title, test = 'test'}: TitleProps) => <h2>{title}{test}</h2>
+
+const App = () => <Title title="test"/>
 
 export default App;
